19a172fbd807a77bb2eda9b5d99c96894dbac7c6
mikecrbenton <mikecrbenton@hotmail.com>
2020-05-06 19:59:57 -0500

    finished except for 2 bugs

--- index.js
+++ index.js
@@ -202,8 +202,20 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+   constructor(myObject){
+      
+      super(myObject);
 
+      this.gradClassName = myObject.gradClassName;
+      this.favInstructor = myObject.favInstructor;
+   }
+   standUp(slack){
+      return `${this.name} announces to ${slack}, @channel standy times!`;
+   }
+   debugsCode(student, subject){
+      return `${this.name} debugs ${student.name}'s code on ${subject}`
+   }
 }
 
 /*
